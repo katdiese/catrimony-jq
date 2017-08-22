0a4ce9745a92e4f7b72bf7823ea656ba0306dda6
Kathy Dieser <katdiese@gmail.com>
2017-08-21 20:50:36 -0600

    reorganized js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,9 @@
-function showClose() {
-  document.getElementById("close").style.display = "block";
-  document.getElementById("mobile-nav").style.display = "block";
-  document.getElementById("hamburger").style.display = "none";
-}
-
-function showHamburger() {
-  document.getElementById("hamburger").style.display = "block";
-  document.getElementById("close").style.display = "none";
-  document.getElementById("mobile-nav").style.display = "none";
-}
-
-document.getElementById("detailsNav").onclick = () => {
-  console.log('vanilla!');
-}
-
-document.getElementById("hamburger").onclick = function() {
-  showClose();
-}
-
-document.getElementById("close").onclick = function() {
-  showHamburger();
-}
-
 function countdownTimer(endtime) {
-  var timeRemaining = Date.parse('2017-12-17') - Date.parse(new Date());
-  var sec = Math.floor((timeRemaining/1000) % 60 );
-  var min = Math.floor((timeRemaining/1000/60) % 60);
-  var h = Math.floor( timeRemaining/(1000*60*60) % 24 );
-  var d = Math.floor( timeRemaining / ( 1000*60*60*24 ));
+  let timeRemaining = Date.parse('2017-12-17') - Date.parse(new Date());
+  let sec = Math.floor((timeRemaining/1000) % 60 );
+  let min = Math.floor((timeRemaining/1000/60) % 60);
+  let h = Math.floor( timeRemaining/(1000*60*60) % 24 );
+  let d = Math.floor( timeRemaining / ( 1000*60*60*24 ));
 
   let dayHtml = document.getElementById('daysRemaining');
   let hourHtml = document.getElementById('hoursRemaining');
@@ -55,6 +31,49 @@ setInterval(function() {
     console.log(countdownTimer('2017-12-17'));
   }
 }, 1000);
+
+// Main Nav Controllers
+function showClose() {
+  document.getElementById("close").style.display = "block";
+  document.getElementById("mobile-nav").style.display = "block";
+  document.getElementById("hamburger").style.display = "none";
+}
+function showHamburger() {
+  document.getElementById("hamburger").style.display = "block";
+  document.getElementById("close").style.display = "none";
+  document.getElementById("mobile-nav").style.display = "none";
+}
+
+// Group of listeners to close hamburger menu
+document.getElementById("detailsNav").onclick = () => {
+  showHamburger();
+}
+document.getElementById("travelNav").onclick = () => {
+  showHamburger();
+}
+document.getElementById("storyNav").onclick = () => {
+  showHamburger();
+}
+document.getElementById("partyNav").onclick = () => {
+  showHamburger();
+}
+document.getElementById("picNav").onclick = () => {
+  showHamburger();
+}
+document.getElementById("regNav").onclick = () => {
+  showHamburger();
+}
+document.getElementById("contactNav").onclick = () => {
+  showHamburger();
+}
+document.getElementById("close").onclick = function() {
+  showHamburger();
+}
+
+// Listener to open hamburger menu
+document.getElementById("hamburger").onclick = function() {
+  showClose();
+}
 
 
 
